3f96555e36213d1c46630c0a20cf080f8e0f593a
Joakim Skjefstad <joakimsk@Joakims-MacBook-Pro.local>
2014-12-22 19:00:08 +0100

    Added HC06 Bluetooth Serial Echo test, needs SPP support

---
+++ mega/hc06_bt/hc06_bt.ino
@@ -0,0 +1,25 @@
+#include <SoftwareSerial.h>
+  
+void setup()
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+ 
+  Serial.println("Ready!");
+ 
+  // set the data rate for the SoftwareSerial port
+ 
+  // for HC-05 use 38400 when poerwing with KEY/STATE set to HIGH on power on
+  Serial1.begin(9600);
+}
+ 
+void loop() // run over and over
+{
+  if (Serial1.available())
+    Serial.write(Serial1.read());
+  if (Serial.available())
+    Serial1.write(Serial.read());
+}
